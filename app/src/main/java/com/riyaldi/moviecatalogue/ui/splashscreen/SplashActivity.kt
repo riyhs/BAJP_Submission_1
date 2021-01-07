799a8e818f6990afa4427e4ef8c6cc6d62e79f7a
Riyaldi Hasan <riy.2923@gmail.com>
2021-01-07 18:27:44 +0700

    add splashscreen and app logo launcher

---
+++ app/src/main/java/com/riyaldi/moviecatalogue/ui/splashscreen/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.riyaldi.moviecatalogue.ui.splashscreen
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import com.riyaldi.moviecatalogue.R
+import com.riyaldi.moviecatalogue.ui.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
